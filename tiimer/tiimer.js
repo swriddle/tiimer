622cfe44651b2fd355391c32201eee54087b485f
Sean Riddle <3479219-tretonin@users.noreply.gitlab.com>
2020-05-31 12:55:44 -0700

    JS start

---
+++ tiimer/tiimer.js
@@ -0,0 +1,21 @@
+function checkStartButtonStatus() {
+    var expr_element = document.getElementById("expr");
+    var content = expr_element.value;
+    console.log("content = '" + content + "'");
+    parse(content);
+}
+
+function parseTime(string) {
+    const timeRegex = /^(?:(\d)+h)?(?:(\d)+m)?(?:(\d)+s)?/;
+    let m = timeRegex.match(string);
+    if
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions(m) {
+    }
+    
+	
+}
+
+function parse(string) {
+    
+    a
+}
