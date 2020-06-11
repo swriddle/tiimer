8cd2ff63fd4becd57a92ae791348579c8744a4af
Sean Riddle <3479219-tretonin@users.noreply.gitlab.com>
2020-06-10 23:03:11 -0700

    Got the objects kind of worked out

--- tiimer/tiimer.js
+++ tiimer/tiimer.js
@@ -1,48 +1,103 @@
-class ParseResult {
-    var state;
-    var value;
-    constructor(state, value) {
-        this.state = state;
-        this.value = value;
-    }
+"use strict";
+
+function createParseResult(state, value) {
+    let parseResult = {
+        state: state,
+        value: value,
+    };
+    return parseResult;
 }
 
-class ParseState {
-    var tokens;
-    var pos;
-    constructor(tokens, pos=0) {
-        this.tokens = tokens;
-        this.pos = pos;
-    }
-    atEnd() {
-        return this.pos == this.tokens.length;
-    }
-    take() {
-        if (this.atEnd()) {
-            return null;
-        }
-        let next = new ParseState(this.tokens, this.pos + 1)
-        let token = this.tokens[this.pos];
-        return ParseResult(next, token);
-    }
+// class ParseResult {
+//     var state;
+//     var value;
+//     constructor(state, value) {
+//         this.state = state;
+//         this.value = value;
+//     }
+// }
 
-    expect(expectedTag) {
-        let result = this.take();
-        if (result == null || result.value.tag != expectedTag) {
-            return null;
-        } else {
-            return result;
+function createParseState(tokens, id, pos=0) {
+    console.log("tokens: =" + tokens + "=");
+    console.log("id: =" + id + "=");
+    let ps = {
+        tokens: tokens,
+        pos: pos,
+        id: id
+    };
+    console.log("ps: " + JSON.stringify(ps));
+    let parseState = {
+        tokens: tokens,
+        pos: pos,
+        id: id,
+        atEnd() {
+            console.log("running atEnd() for id: " + id);
+            return this.pos == this.tokens.length;
+        },
+        take() {
+            if (this.atEnd()) {
+                return null;
+            }
+            let next = createParseState(this.tokens, id + ".next", this.pos + 1)
+            // let next = new ParseState(this.tokens, this.pos + 1)
+            let token = this.tokens[this.pos];
+            return ParseResult(next, token);
+        },
+        expect(expectedTag) {
+            let result = this.take();
+            if (result == null || result.value.tag != expectedTag) {
+                return null;
+            } else {
+                return result;
+            }
+        },
+        peek() {
+            if (this.atEnd()) {
+                return null;
+            } else {
+                return this.tokens[this.pos].tag 
+            }
         }
-    }
-
-    peek() {
-        if (this.atEnd()) {
-            return null;
-        } else {
-            return this.tokens[this.pos].tag 
-        }
-    }
+    };
+    return parseState;
 }
+
+// class ParseState {
+//     var tokens;
+//     var pos;
+//     constructor(tokens, pos=0) {
+//         this.tokens = tokens;
+//         this.pos = pos;
+//     }
+//     atEnd() {
+//         return this.pos == this.tokens.length;
+//     }
+//     take() {
+//         if (this.atEnd()) {
+//             return null;
+//         }
+//         let next = new ParseState(this.tokens, this.pos + 1)
+//         let token = this.tokens[this.pos];
+//         return ParseResult(next, token);
+//     }
+
+//     expect(expectedTag) {
+//         let result = this.take();
+//         if (result == null || result.value.tag != expectedTag) {
+//             return null;
+//         } else {
+//             return result;
+//         }
+//     }
+
+//     peek() {
+//         if (this.atEnd()) {
+//             return null;
+//         } else {
+//             return this.tokens[this.pos].tag 
+//         }
+//     }
+// }
 
 // class TemplateParseException(Exception) {
 // }
@@ -103,7 +158,9 @@ function parseTimeFollowedByConjunctedExpressionAndRepetition(state) {
 }
 
 function parseWholeTemplate(tokens) {
-    let state = new ParseState(tokens);
+    let state = createParseState(tokens, "top");
+    console.log("");
+    // let state = new ParseState(tokens);
     let res = parseTemplate(state);
     if (res == null) {
         throw TemplateParseError("could not parse template");
@@ -119,6 +176,7 @@ function parseTemplate(state) {
     let done = false;
     while (!done) {
         let res = parseSimpleTemplate(state);
+        console.log("simple template parse returned: " + JSON.stringify(res));
         if (res == null) {
             done = true;
         } else {
@@ -194,6 +252,7 @@ function checkStartButtonStatus() {
 
     // try {
         // parse(tokens);
+    console.log("tokenz: " + JSON.stringify(tokens));
         let returned = parseWholeTemplate(tokens);
         console.log("returned value = " + returned);
         enableButton();
@@ -390,7 +449,7 @@ function parseTime(string) {
 function tokenize(string) {
     if (string == "") {
         disableButton();
-        return;
+        return [];
     }
 
     let elements = [];
