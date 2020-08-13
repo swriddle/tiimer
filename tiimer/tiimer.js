89f1d21873bfbd6c9aa2c2c20f3d13b4ef7580e0
Sean Riddle <3479219-tretonin@users.noreply.gitlab.com>
2020-08-12 22:42:49 -0700

    cleanup

--- tiimer/tiimer.js
+++ tiimer/tiimer.js
@@ -1,45 +1,7 @@
 "use strict";
 
-/*
-const nearley = require("nearley");
-const grammar = require("./grammar.js");
-*/
-
-// parses "1m30s" to: [[[null,[[["1"]],"m"],[[["3","0"]],"s"]],[]]]
-
-/*
-function nearleyParse(text) {
-    try {
-        let parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-        parser.feed(text);
-        if (parser.results.length == 0) {
-            return [false, null];
-        } else {
-            return [true, parser.results]
-        }
-    } catch(error) {
-        return [false, null];
-    }
-}
-*/
-
 function checkStartButtonStatus() {
     return false;
-    /*
-    var exprElement = document.getElementById("expr");
-    var content = exprElement.value;
-    var success, response;
-
-    console.log("parsing: '" + content + "'");
-    [success, response] = nearleyParse(content);
-    if (success) {
-        console.log("Response: " + JSON.stringify(response));
-        enableButton();
-    } else {
-        console.log("Could not parse");
-        disableButton();
-    }
-    */
 }
 
 function enableButton() {
@@ -60,170 +22,6 @@ function buttonClick() {
     console.log("click!");
 }
 
-// tokenization below this point.
-
-/*
-function tokenize(string) {
-    if (string == "") {
-        disableButton();
-        return [];
-    }
-
-    let elements = [];
-
-    const watchdog = 5;
-    let watchdogTimer = 0;
-    let parseStart = 0;
-    let keepGoing = true;
-    while (keepGoing) {
-        watchdogTimer += 1;
-        if (watchdogTimer >= watchdog) {
-            console.log("stopped via watchdog");
-            keepGoing = false;
-        }
-        var result, parseStartCandidate, operationName;
-        [result, parseStartCandidate, operationName] = parseStep(string.substring(parseStart));
-        if (result == "ambiguous") {
-            console.log("ambiguous response");
-            keepGoing = false;
-            disableButton();
-        } else if (result == "not-found") {
-            console.log("not-found response");
-            keepGoing = false;
-            disableButton();
-        } else if (result == "fully-parsed") {
-            console.log("fully-tokenized response; All done!");
-            keepGoing = false;
-            enableButton();
-        } else {
-            // console.log("Successfully parsed: " + operationName);
-            // console.log("result: " + JSON.stringify(result));
-            parseStart += parseStartCandidate;
-        }
-        elements.push([operationName, result]);
-    }
-
-    return elements;
-}
-*/
-
-/*
-function parseStep(string) {
-    if (string.length == 0) {
-        return ["fully-parsed", null, null]
-    }
-    console.log("x");
-    let results = [["parseTime", parseTokenTime(string)], ["parseSeparator", parseTokenSeparator(string)], ["parseGroupOpen", parseTokenGroupOpen(string)], ["parseGroupClose", parseTokenGroupClose(string)], ["parseRepetition", parseTokenRepetition(string)]];
-    var matchResult;
-    var matchOperation;
-    var matchConsumedCount;
-    var foundResult = false;
-    var i;
-    for (i = 0; i < results.length; i++) {
-        let operationStuff = results[i];
-        let operationName = operationStuff[0];
-        let operationDetails = operationStuff[1];
-        let consumedCount = operationDetails[1];
-
-        console.log(`opName="${operationName}", matchResult="${matchResult}" consumedCount=${consumedCount}`);
-
-        if (consumedCount != null) {
-            if (foundResult) {
-                console.log("x1");
-	        return ["ambiguous", null, null];
-            } else {
-                console.log("x33");
-                matchResult = operationDetails[0];
-                matchOperation = operationName;
-                matchConsumedCount = consumedCount;
-                foundResult = true;
-            }
-        }
-    }
-    console.log("y");
-
-    if (!foundResult) {
-        return ["not-found", null, null];
-    }
-
-    console.log("returning operation: " + matchOperation);
-    return [matchResult, matchConsumedCount, matchOperation];
-}
-
-function parseTokenGroupOpen(string) {
-    const groupOpenRegex = /^\(/;
-    let m = groupOpenRegex.exec(string);
-    if (m) {
-        return [true, 1];
-    } else {
-        return [null, null];
-    }
-}
-
-function parseTokenGroupClose(string) {
-    const groupCloseRegex = /^\)/;
-    let m = groupCloseRegex.exec(string);
-    if (m) {
-        return [true, 1];
-    } else {
-        return [null, null];
-    }
-}
-
-function parseTokenRepetition(string) {
-    const repetitionRegex = /^x(\d+)/;
-    let m = repetitionRegex.exec(string);
-    if (m) {
-        console.log(JSON.stringify(m));
-        return [m[1], m[0].length];
-    } else {
-        return [null, null];
-    }
-}
-
-function parseTokenSeparator(string) {
-    const separatorRegex = /^,/;
-    let m = separatorRegex.exec(string);
-    if (m) {
-        return [true, 1];
-    } else {
-        return [null, null];
-    }
-}
-
-function parseTokenTime(string) {
-    const timeRegex = /^((\d+)h)?((\d+)m)?((\d+)s)?/;
-    let m = timeRegex.exec(string);
-    if (m) {
-        let hour = m[2];
-        let minute = m[4];
-        let second = m[6];
-        if (hour == null && minute == null && second == null) {
-            return [null, null];
-        }
-        if (hour == null) {
-            hour = 0;
-        } else {
-            hour = parseInt(hour, 10);
-        }
-        if (minute == null) {
-            minute = 0;
-        } else {
-            minute = parseInt(minute, 10);
-        }
-        if (second == null) {
-            second = 0;
-        } else {
-            second = parseInt(second, 10);
-        }
-        return [[hour, minute, second], m[0].length];
-    } else {
-        return [null, null];
-    }
-}
-*/
-
-
 function Duration(hours, minutes, seconds) {
     this.hours = hours;
     this.minutes = minutes;
@@ -237,20 +35,6 @@ Duration.prototype.toString = function() {
     return `Duration(${hourString}H${minuteString}M${secondString}S)`;
 }
 
-/*
-class Duration
-  def initialize(hours, minutes, seconds)
-    @hours = hours
-    @minutes = minutes
-    @seconds = seconds
-  end
-
-  def to_s
-    "#{@hours.nil? ? 0 : @hours}H #{@minutes.nil? ? 0 : @minutes}M #{@seconds.nil? ? 0 : @seconds}S"
-  end
-end
-*/
-
 function ParenthesizedExpression(expr) {
     this.expr = expr;
 }
@@ -258,38 +42,6 @@ function ParenthesizedExpression(expr) {
 ParenthesizedExpression.prototype.toString = function() {
     return "[ " + this.expr + " ]";
 }
-
-/*
-class ParenthesizedExpression
-  def initialize(expr)
-    @expr = expr
-  end
-
-  def to_s
-    "[ #{@expr} ]"
-  end
-end
-*/
-
-function Number(expr) {
-    this.expr = expr;
-}
-
-Number.prototype.toString = function() {
-    return "## " + this.expr + " ##";
-}
-
-/*
-class Number
-  def initialize(expr)
-    @expr = expr
-  end
-
-  def to_s
-    "## #{@expr} ##"
-  end
-end
-*/
 
 function Repetition(expr, count) {
     this.expr = expr;
@@ -300,19 +52,6 @@ Repetition.prototype.toString = function() {
     return this.expr + " X " + this.count;
 }
 
-/*
-class Repetition
-  def initialize(expr, count)
-    @expr = expr
-    @count = count
-  end
-
-  def to_s
-    "#{@expr} X #{@count}"
-  end
-end
-*/
-
 function Conjunction(lhs, rhs) {
     this.lhs = lhs;
     this.rhs = rhs;
@@ -322,28 +61,10 @@ Conjunction.prototype.toString = function() {
     return `Conjunction(${this.lhs}, ${this.rhs})`;
 }
 
-/*
-class Conjunction
-  def initialize(lhs, rhs)
-    @lhs = lhs
-    @rhs = rhs
-  end
-
-  def to_s
-    "#{@lhs} , #{@rhs}"
-  end
-end
-*/
-
 function parseLeftParen(tokens) {
-    console.log("parse left paren");
-    console.log("is array?: " + Array.isArray(tokens));
-    console.log("length: " + tokens.length);
     if (!Array.isArray(tokens) || (Array.isArray(tokens) && tokens.length == 0)) {
         return null;
     }
-    console.log("stuff is left");
-    console.log("tokens[0]: " + JSON.stringify(tokens[0]));
     if (tokens[0][0] == "left_paren") {
         return [tokens.slice(1), "left_paren"];
     } else {
@@ -400,7 +121,6 @@ function parseNumber(tokens) {
         return null;
     }
     if (tokens[0][0] == "number") {
-        console.log("XXXXX: " + tokens[0][1]);
         return [tokens.slice(1), tokens[0][1]];
     } else {
         return null;
@@ -659,5 +379,3 @@ function parseExpression(tokens) {
     }
     return [tokens, lhs];
 }
-
-// module.exports = {checkStartButtonStatus: checkStartButtonStatus, buttonClick: buttonClick, performDaTest: performDaTest}
